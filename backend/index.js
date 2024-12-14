2d80c82a799b7c40454827a67907f3d37159c21d
SYED AHAMMAD BUKKAPATNAM <sarwathsultana308@gmail.com>
2024-12-14 10:51:08 +0530

    cookie session management commit

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV !== "production"){
+if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
 const express = require('express')
@@ -15,15 +15,15 @@ const ExpressError = require('./utils/ExpressError')
 const schedulerRoutes = require('./routes/duty')
 const userRoutes = require('./routes/users')
 const port = process.env.PORT || 4000;
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors({
-    origin: 'https://night-duty-scheduler.onrender.com', 
+    origin: 'https://night-duty-scheduler.onrender.com',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
 }));
-const dbUrl=process.env.DB_URL
+const dbUrl = process.env.DB_URL
 // 'mongodb://127.0.0.1:27017/nds2'
 mongoose.connect(dbUrl)
 const _dirname = path.resolve()
@@ -31,27 +31,27 @@ const _dirname = path.resolve()
 
 const store = MongoStore.create({
     mongoUrl: dbUrl,
-    touchAfter:24*60*60,
-    crypto :{
-        secret :'sadjkmcl'
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret: 'sadjkmcl'
     }
 
 })
 
-store.on("error",function(err){
-    console.log("Error storing",err);
+store.on("error", function (err) {
+    console.log("Error storing", err);
 })
 const sessionConfig = {
     store,
     name: 'session',
     secret: process.env.SECRET,
     resave: false,
-    saveUninitialized: false, 
+    saveUninitialized: false,
     cookie: {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production', 
-        sameSite: 'lax', 
-        maxAge: 1000 * 60 * 60 * 24 * 30 
+        secure: app.get('env') === 'production' ? true : false,
+        sameSite: 'none', 
+        maxAge: 1000 * 60 * 60 * 24 * 30
     }
 };
 app.use(session(sessionConfig))
@@ -63,15 +63,15 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser());
 
-app.use('/',userRoutes)
-app.use('/scheduler',schedulerRoutes)
+app.use('/', userRoutes)
+app.use('/scheduler', schedulerRoutes)
 
-app.use(express.static(path.join(_dirname,'/frontend/dist')))
-app.get('*',(_,res)=>{
-    res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"));
+app.use(express.static(path.join(_dirname, '/frontend/dist')))
+app.get('*', (_, res) => {
+    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
 })
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+})
 
 
